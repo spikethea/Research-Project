9992b65c66c2b9faea998a3004bbab496777b390
QuinceGR <57224107+spikethea@users.noreply.github.com>
2026-06-12 09:54:54 +0100

    Added transform player spwan to Recenter XR postion script, changed charactercontroller height

---
+++ Assets/Scripts/LaneSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class LaneSystem : MonoBehaviour
+{
+    [SerializeField] private EndlessRunnerEmitter emitter;
+    public int numOfLanes;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
